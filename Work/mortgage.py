f4e8d371e9de839841b0705babc6f68045e51f71
Shawnna Cabanday <shawnnacab@yahoo.com>
2023-04-01 16:32:03 -0700

    Adding mortgage.py and pcost.py

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,27 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+month = 0
+
+extra_payment = 1000
+extra_payment_start_month = 5
+extra_payment_end_month = extra_payment_start_month + 4
+
+while principal > 0:
+    month += 1
+    principal = principal * (1+rate/12) - payment
+    if principal > 0:
+        total_paid = total_paid + payment
+        
+        if month >= extra_payment_start_month and month <= extra_payment_end_month:
+            principal = principal * (1+rate/12) - payment - extra_payment
+            total_paid = total_paid + payment + extra_payment
+        
+        print(f"{month:d} {total_paid:.2f} {principal:0.2f}")
+
+print('Total paid', round(total_paid,2))
+print('Months', month)
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+total_cost = 0.0
+for line in f:
+    row = line.split(',')
+    num_shares = int(row[1])
+    price_per_stock = float(row[2].strip())
+    total_cost = total_cost + (num_shares * price_per_stock)
+f.close()
+print("TOTAL COST:", total_cost)

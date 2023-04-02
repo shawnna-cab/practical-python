1ab1167449d545670e7bd727214f270b666d8646
Shawnna Cabanday <shawnnacab@yahoo.com>
2023-04-01 20:02:16 -0700

    Uploading exercise 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,13 +1,29 @@
 # pcost.py
 #
 # Exercise 1.27
-f = open('Data/portfolio.csv', 'rt')
-headers = next(f).split(',')
-total_cost = 0.0
-for line in f:
-    row = line.split(',')
-    num_shares = int(row[1])
-    price_per_stock = float(row[2].strip())
-    total_cost = total_cost + (num_shares * price_per_stock)
-f.close()
-print("TOTAL COST:", total_cost)
+import sys
+
+def portfolio_cost(filename):
+    f = open(filename, 'rt')
+    headers = next(f).split(',')
+    total_cost = 0.0
+    for line in f:
+        row = line.split(',')
+
+        try:
+            num_shares = int(row[1])
+        except ValueError as error:
+            print(error)
+
+        price_per_stock = float(row[2].strip())
+        total_cost = total_cost + (num_shares * price_per_stock)
+    f.close()
+    return total_cost
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)

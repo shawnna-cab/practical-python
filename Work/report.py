38fa6d7d3d4e2e7b2c51a741ed3dc17c5e3d5b48
Shawnna Cabanday <shawnnacab@yahoo.com>
2023-04-02 10:18:24 -0700

    Uploading exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -41,11 +41,23 @@ def read_prices(filename):
 portfolio = read_portfolio('./Data/portfolio.csv')
 prices = read_prices('./Data/prices.csv')
 
-total_value = 0.0 
-total_cost = 0.0
-for stock in portfolio:
-    total_value += stock['shares'] * prices[stock['name']]
-    total_cost += stock['shares'] * stock['price']
+# total_value = 0.0 
+# total_cost = 0.0
+# for stock in portfolio:
+#     total_value += stock['shares'] * prices[stock['name']]
+#     total_cost += stock['shares'] * stock['price']
 
-print(f'Current value: {total_value:0.2f}')
-print(f'Gain/Loss: {total_cost - total_value:0.2f}')
+# print(f'Current value: {total_value:0.2f}')
+# print(f'Gain/Loss: {total_cost - total_value:0.2f}')
+
+def make_report(portfolio, prices):
+    headers = ('Name', 'Shares', 'Price', 'Change')
+    print(f"{headers[0]:>10} {headers[1]:>10} {headers[2]:>10} {headers[3]:>10}")
+    print(("-" * 10 + " ") * len(headers))
+    report = []
+    for stock in portfolio:
+        change = float(prices[stock['name']]-stock['price'])
+        report.append((stock['name'], stock['shares'], round(prices[stock['name']],2), round(change,2)))
+        price = '$' + str(round(prices[stock['name']],2))
+        print(f"{stock['name']:>10s} {stock['shares']:>10d} {price:>10s} {change:>10,.2f}")
+    return report
